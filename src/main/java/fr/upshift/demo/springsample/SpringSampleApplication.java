3f1bdedeee9a6c6461f755ffe777adb145a6e818
Laurent Paumier <laurent.paumier@fnac.net>
2022-05-02 20:54:00 +0200

    init sample app

---
+++ src/main/java/fr/upshift/demo/springsample/SpringSampleApplication.java
@@ -0,0 +1,13 @@
+package fr.upshift.demo.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/upshift/demo/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upshift.demo.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
